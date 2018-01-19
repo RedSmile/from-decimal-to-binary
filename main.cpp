1a7eec9adcd8977dcfb1b92278dc59a104095a7f
RedSmile <34597036+RedSmile@users.noreply.github.com>
2018-01-19 19:21:10 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int number;
+
+void decimale();
+
+int main() {
+
+	decimale();
+
+	system("pause");
+	return 0;
+}
+
+void decimale() {
+	int resto;
+	const int SIZE = 8;
+	int binario[SIZE];
+	int i = 0;
+
+	cout << "DECIMALE A BINARIO" << endl;
+	cout << "inserisci un numero da convertire (max 255):\t";
+	cin >> number;
+
+	for (; i < SIZE; i++)
+	{
+		binario[i] = number % 2;
+		number /= 2;
+		cout << binario[i];
+		if (number == 1 || number == 0) {
+			break;
+		}
+	}
+	cout << endl;
+	for (; i > -1; i--)
+	{
+		cout << binario[i];
+	}
+	cout << endl;
+}
